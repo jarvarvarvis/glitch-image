2dc175bf14d17c039c2d45f0724d922cb365628d
jarvarvarvis <jarvistrigo@gmail.com>
2023-09-11 00:47:25 +0200

    Add error modal and did a bit more input validation

--- pages/api/v1/glitch.js
+++ pages/api/v1/glitch.js
@@ -1,6 +1,8 @@
 import { formidable } from "formidable";
 import { promises as fs } from "fs";
 
+import { MAX_UPLOAD_FILE_SIZE_MB } from "@/constants";
+
 export const config = {
     api: {
         bodyParser: false
@@ -14,24 +16,39 @@ export default async function handler(req, res) {
         });
     }
 
-    const data = await new Promise((resolve, reject) => {
-        const form = formidable({});
-
-        console.log("Parsing request");
-        form.parse(req, (err, fields, files) => {
-            if (err) reject({ err });
-            resolve({ err, fields, files })
+    try {
+        const data = await new Promise((resolve, reject) => {
+            const form = formidable({
+                maxFileSize: MAX_UPLOAD_FILE_SIZE_MB * 1024 * 1024
+            });
+    
+            console.log("Parsing request...");
+            form.parse(req, (err, fields, files) => {
+                if (err) 
+                    reject({ err });
+                resolve({ err, fields, files });
+                console.log("Done parsing request.");
+            });
         });
-    });
 
-    var file = data.files.file[0];
-    var imageBytes = await fs.readFile(file.filepath);
-    console.log("Received data:");
-    console.log(imageBytes);
-    fs.rm(file.filepath); // Remove file
+        var file = data.files.file[0];
+        var imageBytes = await fs.readFile(file.filepath);
+        console.log("Received data:");
+        console.log(imageBytes);
+        fs.rm(file.filepath); // Remove file
 
-    res.status(200).json({
-        status: "OK",
-        imageBytes,
-    });
+        res.status(200).json({
+            status: "OK",
+            imageBytes,
+        });
+    } catch (err) {
+        var error = err.err;
+        console.log(error.message);
+
+        res.status(error.httpCode).json({
+            error: error,
+            errorMessage: error.message
+        });
+        return;
+    }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,17 @@
 
 import { useState } from 'react';
 
+import { MAX_UPLOAD_FILE_SIZE_MB } from '@/constants';
+import ReactModal from 'react-modal';
+
 export default function Home() {
     const [image, setImage] = useState(null);
     const [imageURL, setImageURL] = useState(null);
     const [isLoading, setLoading] = useState(false);
 
+    const [errorMessage, setErrorMessage] = useState("");
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    
     function resetImage() {
         setImage(null);
         setImageURL(null);
@@ -22,6 +28,8 @@ export default function Home() {
         // Files array exists
         if (!event.target.files) {
             resetImage();
+            setErrorMessage("No files were selected");
+            setModalIsOpen(true);
             return;
         }
 
@@ -29,12 +37,16 @@ export default function Home() {
         var file = event.target.files[0];
         if (!file) {
             resetImage();
+            setErrorMessage("No files or file input was invalid were selected");
+            setModalIsOpen(true);
             return;
         }
 
         // The file's mime type is correct (PNG or JPG image)
         if (file.type != "image/png" && file.type != "image/jpeg") {
             resetImage();
+            setErrorMessage("Selected file doesn't have supported type (supported: png, jpg/jpeg)");
+            setModalIsOpen(true);
             return;
         }
 
@@ -68,30 +80,74 @@ export default function Home() {
             }).then(async (response) => {
                 var responseText = await response.text();
                 var responseData = JSON.parse(responseText);
+                
+                // If the request to the API was not successful, inform
+                // the user about the error.
+                if (response.status != 200) {
+                    setLoading(false);
+                    console.error(responseData.errorMessage);
+                    setErrorMessage(responseData.errorMessage);
+                    setModalIsOpen(true);
+                    return;
+                }
 
+                // Get the image data from the response and create a blob
+                // that will then be downloaded.
                 var imageBytes = responseData.imageBytes.data;
                 var imageArray = new Uint8Array(imageBytes, imageBytes.length);
                 var blob = new Blob([imageArray], {
                     type: image.type
                 });
+
+                console.log("Got data of length " + imageBytes.length + ", now downloading.");
                 download(blob, image.name);
                 setLoading(false);
+            }).catch(err => {
+                console.error(String(err));
+                setErrorMessage(String(err));
+                setModalIsOpen(true);
             });
         }
     }
-    
+
     return (
         <div className="font-sans">
             <div className="relative">
                 <img src={imageURL}></img>
             </div>
+            
+            <ReactModal
+                className="
+                    absolute 
+                    top-1/2 left-1/2 
+                    grid justify-center items-center 
+                    translate-x-[-50%] translate-y-[-50%] 
+                    space-y-3 p-3 
+                    bg-neutral-900
+                    rounded-xl
+                "
+                style={{
+                    overlay: {
+                        backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    }
+                }}
+                isOpen={modalIsOpen}
+            >
+                <h1 className="text-lg font-semibold">Error</h1>
+                <p>{errorMessage}</p>
+                <button className="font-medium items-center justify-center h-9 px-6 rounded-md text-slate-300 border border-slate-200" onClick={
+                    _ => {
+                        setModalIsOpen(false);
+                    }
+                }>Close</button>
+            </ReactModal>
 
             <form className="flex-auto p-6 space-y-5">
                 <h1 className="flex-auto text-lg font-semibold">
                     Select Image
                 </h1>
                 
-                <div className="space-x-1">
+                <div className="space-x-2">
                     <input
                         id="fileInput"
                         type="file" 
@@ -99,7 +155,7 @@ export default function Home() {
                         disabled={isLoading} 
                         onChange={onUpdateClientImage}
                     ></input>
-                    <a>(Upload size: 5MB)</a>
+                    <a>(Upload size: {MAX_UPLOAD_FILE_SIZE_MB}MB)</a>
                 </div>
 
                 <div className="space-x-2">
